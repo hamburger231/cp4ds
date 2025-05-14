fe55771db08ccf0ff8cb48aaf6b6e1ae441e9593
hamburger231 <david.s17q@gmail.com>
2025-05-14 12:41:16 +0200

    Lab08

---
+++ src/main/java/pl/szmitka/ecommerce/sales/AcceptOfferCommand.java
@@ -0,0 +1,7 @@
+package pl.szmitka.ecommerce.sales;
+
+public class AcceptOfferCommand {
+    String fname;
+    String lname;
+    String email;
+}
---
+++ src/main/java/pl/szmitka/ecommerce/sales/SalesFacade.java
@@ -0,0 +1,13 @@
+package pl.szmitka.ecommerce.sales;
+
+public class SalesFacade {
+    public void addToCart(String productId) {
+
+    }
+    public void acceptOffer(AcceptOfferCommand acceptOfferCommand) {
+
+    }
+    public void makeReservationPaid(String reservationId) {
+
+    }
+}
